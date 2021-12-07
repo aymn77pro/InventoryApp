d7bd8e1576a014969100cd995f7972af221fa78b
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-12-07 22:06:48 +0300

    inventory app

--- app/src/main/java/com/example/inventory/data/ItmeDao.kt
+++ app/src/main/java/com/example/inventory/data/ItmeDao.kt
@@ -1,4 +1,23 @@
 package com.example.inventory.data
 
-class ItmeDao {
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItmeDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(itme: Item)
+
+    @Update
+    suspend fun update(itme: Item)
+
+    @Delete
+    suspend fun delete(itme: Item)
+
+    @Query("SELECT * from item ORDER BY name ASC")
+    fun getAll():Flow<List<Item>>
+
+    @Query("SELECT * from item WHERE id = :id")
+    fun getItem(id: Int):Flow<Item>
 }
